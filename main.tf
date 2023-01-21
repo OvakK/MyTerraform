f4a977c21c31509049591233dc670811fe1cb768
OvakK <120728700+OvakK@users.noreply.github.com>
2023-01-21 13:48:23 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.0.0/24"
+  cidr_block        = "172.16.10.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
